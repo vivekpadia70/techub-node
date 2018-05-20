8c928c264390d66e3a5d2979d7df159fab1009cb
vivek padia <vivekpadia70@gmail.com>
2018-05-20 19:23:07 +0530

    commti

--- app.js
+++ app.js
@@ -38,10 +38,9 @@ app.use(session({
   saveUninitialized: true,
   cookie: { secure: true }
 }));
-app.all('/', function(req, res, next) {
+app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("ACcess-Control-Allow-Methods", "GET, PUT, POST, DELETE");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
  });
 
